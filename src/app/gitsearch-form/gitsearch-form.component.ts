ae537a4fa87717c57797c6d95e60965f7a670cc4
Hulian2000 <prepmoringa11@gmail.com>
2019-08-24 22:46:21 +0300

    This commit will add a bootsrap cdn link

--- src/app/gitsearch-form/gitsearch-form.component.ts
+++ src/app/gitsearch-form/gitsearch-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-gitsearch-form',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gitsearch-form.component.css']
 })
 export class GitsearchFormComponent implements OnInit {
+
+  @Output () emitUser = new EventEmitter<any>()
+
+  gitUser: string;
+  user(){
+    console.log(this.gitUser)
+    this.emitUser.emit(this.gitUser)
+  }
 
   constructor() { }
 
