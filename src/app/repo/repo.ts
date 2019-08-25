6559c00b718e174ae999d8f1b3eb50df58124aa1
Hulian2000 <prepmoringa11@gmail.com>
2019-08-25 11:10:33 +0300

    This commit places different data types in the classes

--- src/app/repo/repo.ts
+++ src/app/repo/repo.ts
@@ -1,2 +1,3 @@
 export class Repo {
+    constructor(public name:string, public html_url:string,public description:string,public language:string){}
 }
--- src/app/users/user.ts
+++ src/app/users/user.ts
@@ -1,2 +1,3 @@
 export class User {
+    constructor(public avatar_url:string,public bio:string,public followers_url:string,public following_url:string,public name:string, login:string){}
 }
